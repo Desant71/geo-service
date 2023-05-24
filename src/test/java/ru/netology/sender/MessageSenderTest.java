b44bdc86b91bb2a7fcd184c308542257f32090c6
Desant71 <desant31081993@gmail.com>
2023-05-25 02:57:31 +0300

    Добавил 2 теста MessageSender

---
+++ src/test/java/ru/netology/sender/MessageSenderTest.java
@@ -0,0 +1,58 @@
+package ru.netology.sender;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import ru.netology.entity.Country;
+import ru.netology.entity.Location;
+import ru.netology.geo.GeoService;
+import ru.netology.i18n.LocalizationService;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MessageSenderTest {
+    private GeoService geoService;
+    private LocalizationService localizationService;
+    private Map<String, String> headers;
+    private MessageSender messageSender;
+
+    @BeforeEach
+    void setUp() {
+        geoService = Mockito.mock(GeoService.class);
+        localizationService = Mockito.mock(LocalizationService.class);
+        messageSender = new MessageSenderImpl(geoService, localizationService);
+        headers = new HashMap<>();
+    }
+
+
+    @Test
+    public void testByRussia() {
+        Mockito.when(geoService.byIp("172."))
+                .thenReturn(new Location(null, Country.RUSSIA, null, 0));
+        Mockito.when(localizationService.locale(Country.RUSSIA))
+                .thenReturn("Добро пожаловать");
+
+        headers.put(MessageSenderImpl.IP_ADDRESS_HEADER, "172.");
+        String actual = messageSender.send(headers);
+        String expected = localizationService.locale(Country.RUSSIA);
+
+        Assertions.assertEquals(expected, actual);
+    }
+    @Test
+    public void testByUSA() {
+        Mockito.when(geoService.byIp("96."))
+                .thenReturn(new Location(null, Country.USA, null, 0));
+        Mockito.when(localizationService.locale(Country.USA))
+                .thenReturn("Welcome");
+
+        headers.put(MessageSenderImpl.IP_ADDRESS_HEADER, "96.");
+        String actual = messageSender.send(headers);
+        String expected = localizationService.locale(Country.USA);
+
+        Assertions.assertEquals(expected, actual);
+    }
+}
